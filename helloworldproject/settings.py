423212e2e1ded12c40fc56e1da749b6dbe2d166d
pis9popa <vedenyov10@gmail.com>
2025-06-24 09:19:45 +0500

    upd

--- helloworldproject/settings.py
+++ helloworldproject/settings.py
@@ -228,7 +228,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'vedenyov10@gmail.com'
-EMAIL_HOST_PASSWORD = 'wsiw piwu szpa vbnx'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'vedenyov10@gmail.com'
 
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3
